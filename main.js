65a2fe80a9c9a18fbd137576484428aeca39c5d5
Benjamin Rubin <bennyfrubin@gmail.com>
2018-05-24 10:06:58 +0300

    ש

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function navigate (button) {
+
+	if (button.id == "about") {
+		document.getElementById("home").style.display = "none";
+		document.getElementById("aboutMe").style.display = "block";
+
+
+	}
+	else if (button.id == "work") {
+		
+
+
+	}
+
+
+}
